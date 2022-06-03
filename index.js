ef9cf26933d64acc3947a255cc4f6b9dc23fb347
Edyta <ulanowicz.edyta@gmail.com>
2022-06-03 22:11:00 +0200

    reformat index.js

--- index.js
+++ index.js
@@ -6,19 +6,19 @@ console.log(`Choose your number from 1 to 10`);
 
 prompt.start();
 
-prompt.get('number', function (_err, result) {
+prompt.get('number', (_err, result) => {
 
     const userNr = parseInt(result.number);
 
     let message = () => {
-            if (userNr < 1 || userNr > 10)
-                { return msg= `Incorrect number!` };
-
-            if (userNr >= 1 && userNr <= 10 &&  userNr === randomNr)
-                 { return msg = `Success! Your number ${userNr} = compared number ${randomNr}` };
-
-                 { return msg = `Try again! Your number ${userNr}; compared number ${randomNr}` };
-            };
+            let msg;
+            if (userNr < 1 || userNr > 10) {
+                return msg= `Incorrect number!`
+            }; if (userNr >= 1 && userNr <= 10 &&  userNr === randomNr) {
+                return msg = `Success! Your number ${userNr} = compared number ${randomNr}`
+            }; {
+                return msg = `Try again! Your number ${userNr}; compared number ${randomNr}`
+            };};
 
     console.log(message())
 } );
